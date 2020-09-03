6f76cbd36c6f8b9c69a357e6524cf2e918085bd4
Pete Fitton <petefitton@gmail.com>
2020-09-03 15:08:26 -0700

    Add About and Footer components, plus commit general react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Welcome from './components/Welcome';
+import About from './components/About';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <About />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is all about us.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Voluptate laboriosam illo repudiandae quo! Vero dolorum placeat 
+        aliquam recusandae sed veritatis quos, eum, voluptate nulla 
+        fugiat nemo iusto hic eaque quidem.
+      </p>
+    </div>
+  )
+}
+
+export default About;
